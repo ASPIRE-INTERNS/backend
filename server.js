77a57f82e37489268c60ecd61e37d567db581093
varshiinii <se21uari184@mahindrauniversity.edu.in>
2025-05-08 16:27:50 +0530

    Updated

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000' 
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
